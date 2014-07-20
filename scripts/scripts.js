2ac3216d8cce7495266707780608a0dd3b069d21
Bryan Irace <bryan.irace@gmail.com>
2014-07-20 17:14:59 -0400

    Cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,5 @@ $(function() {
     offset: 60
   });
 
-  $(function() {
-    $('#days-until').text(Math.round((new Date(2015, 4, 23) - new Date()) / 1000/60/60/24));
-  });
+  $('#days-until').text(Math.round((new Date(2015, 4, 23) - new Date()) / 1000/60/60/24));
 });
