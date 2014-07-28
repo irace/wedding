96c86f1ccee2a4b5c756dd678f560f5494fca3f2
Bryan Irace <bryan.irace@gmail.com>
2014-07-27 22:35:20 -0400

    More playing around

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,8 @@ $(function() {
   });
 
   $('#days-until').text(Math.round((new Date(2015, 4, 23) - new Date()) / 1000/60/60/24));
+
+  $.getJSON('http://gramcracker.heroku.com/tag/shakeyourboudouvas', function(data) {
+    alert(data);
+  });
 });
