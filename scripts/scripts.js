89a19fc429ba2e2515d1c2efd3913b372e95f9f7
Bryan Irace <bryan.irace@gmail.com>
2014-08-23 23:26:50 -0400

    ASCII art, drop shadow

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,30 @@
 $(function() {
+  enable_sticky_navigation();
+  populate_days_until();  
+  fetch_instagram_photos();
+  attach_nav_scroll_handlers();
+});
+
+function enable_sticky_navigation() {
   $('.nav').waypoint('sticky', {
     offset: 60
   });
+}
 
+function populate_days_until() {
   $('#days-until').text(Math.round((new Date(2015, 4, 23) - new Date()) / 1000/60/60/24));
+}
 
-  fetch_instagram_photos();
-});
+function attach_nav_scroll_handlers() {
+  // Copied from Zack
+  $('.nav a[href*=#]').bind('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).attr("href");
+    //animation
+    $('body, html').animate({ scrollTop: $(target).offset().top }, 700, 'swing', function () {});
+  });  
+}
 
 function fetch_instagram_photos() {
   var url_blacklist = [
