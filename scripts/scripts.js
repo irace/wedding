bfce585e1460cc205e8d73355c4aefced51eef78
Bryan Irace <bryan.irace@gmail.com>
2014-08-24 20:01:39 -0400

    Resize Instagram photos on window resize

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 function enable_sticky_navigation() {
   if (window.matchMedia("(min-width: 768px)").matches) {
     // Copied from Zack
-    
+
     $('.nav').waypoint('sticky', {
       offset: 60
     });
@@ -50,12 +50,17 @@ function fetch_instagram_photos() {
       }
     });
 
-    $('.photo-col').each(function (index) {
-      var $el = $(this);
+    var make_photo_cols_square = function () {
+      $('.photo-col').each(function (index) {
+        var $el = $(this);
 
-      // This won't update when say, a browser window is resized
-      $el.css({ 'height': $el.width() + 'px' });
-    });
+        // This won't update when say, a browser window is resized
+        $el.css({ 'height': $el.width() + 'px' });
+      });
+    };
+
+    make_photo_cols_square();
+    $(window).resize(make_photo_cols_square);
   });
 }
 
